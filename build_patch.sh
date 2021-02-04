212dd58e418768c1c5d06d85f1386ae69a6d2f28
rejigtian <875283604@qq.com>
2021-02-04 16:30:28 +0800

    开启混淆

--- build_patch.sh
+++ build_patch.sh
@@ -1,30 +1,41 @@
-FIX_BRANCH="hotfix"
-BASE_BRANCH="master"
 BASE_APK="official/release/base-app.apk"
+MAPPING="official/release/base-app-mapping.apk"
+R="official/release/base-app-R.apk"
+APK_LOCATION="master"
 
-
-echo "-- 构建热修复补丁包,请确认分支存在 --"
+echo "-- 开始构建热修复补丁包 --"
 for i in "$@"; do
     echo $i
 done
 
 if [[ -n "$1" ]]; then
-  FIX_BRANCH="$1"
-  if [[ -n "$2" ]]; then
-      BASE_BRANCH="$2"
-       else
-          echo "未传入基线分支，使用当前分支作为基线"
-  fi
-  echo "目前使用基线分支 {$BASE_BRANCH}， 使用修复分支 {$FIX_BRANCH}"
+  BASE_APK="$1"
   else
-    echo "未传入修复分支与基线分支，默认使用基线分支 {$BASE_BRANCH}， 使用修复分支 {$FIX_BRANCH}"
+    echo "参数1：是否开启混淆0关闭，1开启；参数2：安装包路径；[参数3：mapping文件路径]；[参数4：R文件路径]；[参数5：分支]"
+    exit
 fi
 
-git checkout $BASE_BRANCH
-rm -r -f official/release/*
-./gradlew assembleRelease -P BUILD_PATCH=true
-cp -f official/release/baseapp/baseapp-release.apk BASE_APK
-git checkout $FIX_BRANCH
+if [[ -n "$2" ]]; then
+  APK_LOCATION="$2"
+  else
+    echo "参数1：是否开启混淆0关闭，1开启；参数2：安装包路径；[参数3：mapping文件路径]；[参数4：R文件路径]；[参数5：分支]"
+    exit
+fi
+
+if [[ -n "$3" ]]; then
+  cp -f "$3" MAPPING
+fi
+
+if [[ -n "$4" ]]; then
+  cp -f "$4" R
+fi
+
+if [[ -n "$5" ]]; then
+  git checkout "$5"
+fi
+
+rm -r -f official/release/base-app*
+cp -f APK_LOCATION BASE_APK
 ./gradlew buildTinkerPatchRelease
 cp -f -a app/build/outputs/patch official/release/patch
 echo "-- 热修复打包完成，请复核MF文件，保证检查版本信息正确! --"
