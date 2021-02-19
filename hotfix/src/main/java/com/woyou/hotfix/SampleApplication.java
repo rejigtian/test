b8ee13851c4f9ec67a979cf8d0039ef97ebca000
rejigtian <875283604@qq.com>
2021-02-19 17:39:11 +0800

    init

--- hotfix/src/main/java/com/woyou/hotfix/SampleApplication.java
+++ hotfix/src/main/java/com/woyou/hotfix/SampleApplication.java
@@ -6,6 +6,6 @@ import com.tencent.tinker.loader.shareutil.ShareConstants;
 public class SampleApplication extends TinkerApplication {
     public SampleApplication() {
         super(ShareConstants.TINKER_ENABLE_ALL, "com.woyou.hotfix.SampleApplicationLike",
-                "com.tencent.tinker.loader.TinkerLoader", false, false);
+                "com.tencent.tinker.loader.TinkerLoader", false, true);
     }
 }
